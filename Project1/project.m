3d8dd8950a513af7af9116b219592729932498cc
Christoph Koehler <christoph@zerodeviation.net>
2009-11-23 13:37:10 -0600

    add some noise filtering and calibration.

--- Project1/project.m
+++ Project1/project.m
@@ -1,22 +1,36 @@
 % ================
 % = Map the data =
 % ================
+clear;
+load par_el0.5_lprt.mat;
 P1 = pow;
 
 figure(1)
-% % now plot the result.
-% pcolor([x],[y],[P1]);
-% shading flat;
-% axis equal;
-% axis([-350 350 -350 350]);
-% colormap(jet);
-% colorbar;
-% caxis([0 60]);
+% now plot the result.
+pcolor([x],[y],[P1]);
+shading flat;
+axis equal;
+axis([-350 350 -350 350]);
+colormap(jet);
+colorbar;
+caxis([0 60]);
 
 
 % now adjust for range to get reflectivity
-C = 20;
-Z = P1 + 20*log10(r) - C;
+Radjusted = R0;
+Rneg = Radjusted <= 1;
+Radjusted(Rneg) = 0;
+
+Zlin = Radjusted .* r.^2;
+
+Z = 10*log10(Zlin);
+Zneg = Z == -inf;
+Z(Zneg) = 0;
+Zstuff = Z > 0;
+C = 25;
+
+Z(Zstuff) = Z(Zstuff) - C;
+Z(:,1:1500) = 0;
 
 figure(2);
 pcolor([x],[y],[abs(Z)]);
